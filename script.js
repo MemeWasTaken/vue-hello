4ba8977cff84842c4e8eadd578440fcc6438cd9f
MemeWasTaken <memelazzari02@gmail.com>
2021-12-15 16:21:52 +0100

    img

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const app = new Vue(
         el: '#containerVue',
         data: {
             message: 'Questo è il messaggio della consegna',
+            linkImg: 'https://picsum.photos/300/200',
         }
     }
 );
